f8d159f0a1b7173fc4af3948d5e2c5d8849b14ec
flo-collab <f.u.etudes@laposte.net>
2022-02-10 23:03:54 +0100

    Selenium works and path for download also

---
+++ sele.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Uwsing M_edge
+Version 91.0.864.67 (Version officielle) (64 bits)
+"""
+#christophe utilise chrome:
+# from selenium import webdriver
+# driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+# # driver.maximize_window()
+# driver.get('http://51.195.220.149/book.php?id=3')
+
+
+
+import os 
+from msedge.selenium_tools import Edge, EdgeOptions
+
+options = EdgeOptions()
+options.use_chromium = True
+
+data_path = os.path.join(os.getcwd(),'books_folder')
+prefs = {"download.default_directory" :data_path}
+options.add_experimental_option("prefs",prefs)
+
+driver  = Edge(options = options)
+
+
+
+driver.get('http://51.195.220.149/book.php?id=3')
+
+
+
+import time
+driver.get('https://www.browserstack.com/test-on-the-right-mobile-devices');
+downloadcsv= driver.find_element_by_css_selector('.icon-csv');
+gotit= driver.find_element_by_id('accept-cookie-notification');
+gotit.click();    
+downloadcsv.click();
+time.sleep(5)
+driver.close()
