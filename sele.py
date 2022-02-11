0bfb4961c21fd013d23f8473e8f7428068fa5808
flo-collab <f.u.etudes@laposte.net>
2022-02-11 10:34:07 +0100

    scraping some books at the same time

--- sele.py
+++ sele.py
@@ -9,9 +9,8 @@ Version 91.0.864.67 (Version officielle) (64 bits)
 # # driver.maximize_window()
 # driver.get('http://51.195.220.149/book.php?id=3')
 
-
-
-import os 
+import os ,time
+from re import T
 from msedge.selenium_tools import Edge, EdgeOptions
 
 options = EdgeOptions()
@@ -24,7 +23,6 @@ options.add_experimental_option("prefs",prefs)
 driver  = Edge(options = options)
 
 def test_sele():
-    import time
     driver.get('https://www.browserstack.com/test-on-the-right-mobile-devices');
     downloadcsv= driver.find_element_by_css_selector('.icon-csv');
     gotit= driver.find_element_by_id('accept-cookie-notification');
@@ -33,10 +31,28 @@ def test_sele():
     time.sleep(5)
     driver.close()
 
+# driver.get('http://51.195.220.149/book.php?id=3')
 
-driver.get('http://51.195.220.149/book.php?id=3')
-# target = driver.find_element_by_class_name("class_of_element")
-print("---- lala ----","\nWork in progress...")
-target = driver.find_element_by_css_selector('a[download=""]')
-print(target)
-target.click()
+# print("---- lala ----", "\n Work in progress...")
+# target = driver.find_element_by_css_selector('a[download=""]')
+# target.click()
+
+def scrap_one(id:int, sleep=True):
+    print(f'\n --- Scraping book with id = {id} ---')
+    driver.get(f'http://51.195.220.149/book.php?id={id}')
+    target = driver.find_element_by_css_selector('a[download=""]')
+    target.click()
+    if sleep:
+        time.sleep(5)
+
+
+for i in range(5):
+    print("---- lala ----", "\n Work in progress...")
+    scrap_one(i+1,sleep=False )
+
+
+
+
+# for i in range(1165):
+#     print("---- lala ----", "\n Work in progress...")
+
